09e46390de5f1bdcd793db29779c5c8e94827bdd
Katerina <34300426+KattyFox@users.noreply.github.com>
2021-04-12 23:39:39 +0300

    feat: done 04 task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,40 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function mineScan(matrix, x, y) {
+  let value = 0;
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+    for (let j = 0; j < row.length; j++) {
+      if (matrix[i][j]) {
+        if (x === i && Math.abs(y - j) === 1) {
+          value++;
+        }
+
+        if (y === j && Math.abs(x - i) === 1) {
+          value++;
+        }
+
+        if (Math.abs(y - j) === 1 && Math.abs(x - i) === 1) {
+          value++;
+        }
+      }
+    }
+  }
+  return value;
 }
 
+function minesweeper(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    const row = matrix[i];
+    const resultRow = [];
+
+    for (let j = 0; j < row.length; j++) {
+      resultRow.push(mineScan(matrix, i, j));
+    }
+    result.push(resultRow);
+  }
+  return result;
+}
 module.exports = minesweeper;
