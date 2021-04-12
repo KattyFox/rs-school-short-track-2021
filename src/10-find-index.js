fb17b638035ae611d97075cf8205b50d2881b66e
Katerina <34300426+KattyFox@users.noreply.github.com>
2021-04-13 00:51:39 +0300

    feat: done 10 task

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,24 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(sortedArray, key) {
+  let start = 0;
+  let end = sortedArray.length - 1;
+
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+
+    if (sortedArray[middle] === key) {
+      return middle;
+    }
+    if (sortedArray[middle] < key) {
+      start = middle + 1;
+    } else {
+      // search searching to the left
+      end = middle - 1;
+    }
+  }
+  return -1;
 }
 
 module.exports = findIndex;
