dcf0d5117c755c3c167a0339aa7ed460f0163baf
Katerina <34300426+KattyFox@users.noreply.github.com>
2021-04-13 01:50:07 +0300

    feat: done 07 task

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,30 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+
+function renameFiles(names) {
+  const output = [];
+
+  const result = {};
+
+  for (let i = 0; i < names.length; i++) {
+    const newName = names[i];
+
+    if (!result[newName]) {
+      result[newName] = 1;
+
+      if (output.indexOf(newName) !== -1) {
+        output.push(`${newName}(1)`);
+      } else {
+        output.push(newName);
+      }
+    } else {
+      const count = result[newName]++;
+      output.push(`${newName}(${count})`);
+    }
+  }
+
+  return output;
 }
 
 module.exports = renameFiles;
