7ae36af122956041854038638f3f43b30c62fcbc
Katerina <34300426+KattyFox@users.noreply.github.com>
2021-04-13 01:00:31 +0300

    feat: done 11 task

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,31 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let result = '';
+  let inspectChar = str[0] || '';
+  let count = 1;
+  for (let i = 1; i < str.length; i++) {
+    const current = str[i];
+    if (current === inspectChar) {
+      count++;
+    } else {
+      if (count !== 1) {
+        result += String(count);
+      }
+      result += inspectChar;
+      count = 1;
+      inspectChar = current;
+    }
+  }
+
+  if (count !== 1) {
+    result += String(count);
+  }
+
+  result += inspectChar;
+
+  return result;
 }
 
 module.exports = encodeLine;
